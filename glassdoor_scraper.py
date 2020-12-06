dbee2b5150587135f872dc4955c9f19aaf3500cb
pawangoon063 <pawan.goon063@gmail.com>
2020-12-07 04:26:30 +0530

    uploading data collection csv file

--- glassdoor_scraper.py
+++ glassdoor_scraper.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  6 02:02:33 2020
+author: Kenarapfaik
+url: https://github.com/arapfaik/scraping-glassdoor-selenium
+"""
+
 from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
